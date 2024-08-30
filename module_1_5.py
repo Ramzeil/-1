a108b772bb3405e908cef8249827a4d5c4ead1ad
Ramzeil <38637712+Ramzeil@users.noreply.github.com>
2024-08-30 06:19:52 +0500

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,14 @@
 immutable_var = 1, 2, 'a', 'b'
 print('immutable_var:', immutable_var)
-mutable_list = [1, 2, 'a', 'b', 'Modified']
-print('mutable_list:', mutable_list)
-# Кортеж относится к неизменяемым типам данных.
-mutable_list[1][2] = 200
+print(immutable_var[0])
+print(immutable_var[1])
+#mutable_list =[1]=200
+print('Кортеж относится к неизменяемым типам данных.')
+print('Но в себе он может хранить изменяемые объекты, если внутри кортежа будет список.')
+mutable_list=[1, 2, 3, 4, 'a','b']
 print(mutable_list)
-# Но в себе он может хранить изменяемые объекты, если внутри кортежа будет список.
+mutable_list[1]=17
+mutable_list[2]='monkeys'
+mutable_list[3]="elephant"
+mutable_list[4]=99
+print(mutable_list)
