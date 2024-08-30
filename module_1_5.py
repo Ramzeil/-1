164b8903eaf143c9f7e03bc7633a6fd581d02067
Ramzeil <38637712+Ramzeil@users.noreply.github.com>
2024-08-30 05:00:11 +0500

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -2,3 +2,7 @@ immutable_var = 1, 2, 'a', 'b'
 print('immutable_var:', immutable_var)
 mutable_list = [1, 2, 'a', 'b', 'Modified']
 print('mutable_list:', mutable_list)
+# Кортеж относится к неизменяемым типам данных.
+mutable_list[1][2] = 200
+print(mutable_list)
+# Но в себе он может хранить изменяемые объекты, если внутри кортежа будет список.
